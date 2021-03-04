01f5fd08ed3cb4dfcafd275876cc3c7f773fd99d
MrChrisRandall <54892528+MrChrisRandall@users.noreply.github.com>
2021-03-04 13:46:01 +0000

    Add files via upload

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "~<bruYSWKmwUt7YY>*l7?@8=5`<r5C")
+os.environ.setdefault("", "")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")
